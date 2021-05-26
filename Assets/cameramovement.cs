64785bd05fdaf7d32d9f82af51f481aab4bb4ba5
mahanthimunna <mahanthi.kannepally2819@gmail.com>
2021-05-26 17:18:19 +0530

    2D enless game

---
+++ Assets/cameramovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramovement : MonoBehaviour
+{
+    public float cameraspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += new Vector3(cameraspeed * Time.deltaTime, 0, 0);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/loopingbackground.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loopingbackground : MonoBehaviour
+{
+    public float backgroundspeed;
+    public Renderer backgroundrenderer;
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundrenderer.material.mainTextureOffset += new Vector2(backgroundspeed * Time.deltaTime, 0f);
+    }
+}
